3aa8787bc7d0eff89a3c81a229ea1b02c940a321
akashamar <akash.amar96@gmail.com>
2021-12-12 01:01:22 +0530

    prettier

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [imgData, setImgData] = useState('');
+  const [imgData, setImgData] = useState("");
   const [imageCapture, setImageCapture] = useState(null);
   const videoRef = useRef(null);
 
   useEffect(() => {
-    const getMediaDevice = async() => {
+    const getMediaDevice = async () => {
       try {
-        const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true })
+        const mediaStream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
         if (videoRef.current) {
           videoRef.current.srcObject = mediaStream;
           videoRef.current.play();
@@ -17,36 +19,34 @@ function App() {
         const track = mediaStream.getVideoTracks()[0];
         const capabilities = track.getCapabilities();
         const settings = track.getSettings();
-        console.log(capabilities, settings)
+        console.log(capabilities, settings);
         setImageCapture(new ImageCapture(track));
       } catch (err) {
         console.log(err);
       }
-    }
+    };
 
-    getMediaDevice()
+    getMediaDevice();
   }, []);
 
-  const onTakePhotoButtonClick = async() => {
+  const onTakePhotoButtonClick = async () => {
     try {
-      const blob = await imageCapture.takePhoto()
-      const imageBitmap = await createImageBitmap(blob)
+      const blob = await imageCapture.takePhoto();
+      const imageBitmap = await createImageBitmap(blob);
       console.log(`Photo size is ${imageBitmap.width}x${imageBitmap.height}`);
-      setImgData(URL.createObjectURL(blob))
-      console.log(blob)
+      setImgData(URL.createObjectURL(blob));
+      console.log(blob);
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className="App">
       <video ref={videoRef} id="video-track" />
       <div className="right-elements">
-        <img src={imgData} alt="img" className="screenshot"/>
-        <button onClick={onTakePhotoButtonClick}>
-          Take screen shot
-        </button>
+        <img src={imgData} alt="img" className="screenshot" />
+        <button onClick={onTakePhotoButtonClick}>Take screen shot</button>
       </div>
     </div>
   );
